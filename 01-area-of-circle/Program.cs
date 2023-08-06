c2289c0b3b3aa92b629037203ea3a8bab8ae681e
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-06 11:30:14 +0700

    Counting The Area of a circle

--- 01-area-of-circle/Program.cs
+++ 01-area-of-circle/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        string? radius = Console.ReadLine();
+
+        Console.WriteLine(Math.PI * Math.Pow(Convert.ToDouble(radius), 2));
+    }
+}
