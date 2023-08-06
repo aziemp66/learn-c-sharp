a309a4fc5946e93b979435466a7b1f57f39862da
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-06 18:31:51 +0700

    Label in C#

---
+++ 02-go-to/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Start");
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+        myLabel:
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            count++;
+            if (count <= 3)
+            {
+                goto myLabel;
+            }
+        }
+    }
+}
