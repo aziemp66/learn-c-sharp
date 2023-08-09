41aa6f6951b8f0818c2be8eb3b1bd4e445e49d27
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-09 21:11:21 +0700

    Handling & Creating Custome Exception

---
+++ 06-exception/HttpException/UnauthorizedException.cs
@@ -0,0 +1,13 @@
+namespace HttpException
+{
+    class UnauthorizedException : Exception
+    {
+        public UnauthorizedException() { }
+
+        public UnauthorizedException(string message)
+            : base(message) { }
+
+        public UnauthorizedException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
---
+++ 06-exception/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Login(string user)
+    {
+        if (!user.Equals("Azie"))
+        {
+            throw new HttpException.UnauthorizedException("You Are Not Authorized");
+        }
+
+        System.Console.WriteLine("You are Authorized");
+    }
+
+    static void Main(string[] args)
+    {
+        string? user = Console.ReadLine();
+
+        if (user == null)
+        {
+            Console.WriteLine("You are not Logged In");
+            return;
+        }
+
+        try
+        {
+            Login(user);
+        }
+        catch (HttpException.UnauthorizedException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
