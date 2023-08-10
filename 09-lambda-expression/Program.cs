19d9274191b9859fa440c1d0a2ee9ef8cb87b5c4
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-10 15:28:46 +0700

    Learning Lambda Function

---
+++ 09-lambda-expression/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lambda
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("John", 34),
+                new Person("Tony", 22),
+                new Person("Andy", 55)
+            };
+
+            var sortedByAge = people.OrderBy(p => p.Age);
+        }
+    }
+}
