45b9d45c72f72d2b643a05856dddccafe7c7f64b
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-12 20:47:24 +0700

    init Project

---
+++ 12-events-accessors/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ThermostatApp
+{
+    class Program
+    {
+        static void Main(string[] args) { }
+    }
+
+    class HeatSensor : IHeatSensor
+    {
+        public event EventHandler<TemperatureEventArgs> TemperatureReachesEmergencyLevelEventHandler;
+        public event EventHandler<TemperatureEventArgs> TemperatureReachesWarningLevelEventHandler;
+        public event EventHandler<TemperatureEventArgs> TemperatureReachesBelowWarningLevelEventHandler;
+    }
+
+    interface IHeatSensor
+    {
+        event EventHandler<TemperatureEventArgs> TemperatureReachesEmergencyLevelEventHandler;
+        event EventHandler<TemperatureEventArgs> TemperatureReachesWarningLevelEventHandler;
+        event EventHandler<TemperatureEventArgs> TemperatureReachesBelowWarningLevelEventHandler;
+    }
+
+    public class TemperatureEventArgs : EventArgs
+    {
+        public float Temperature { get; set; }
+        public DateTime CurrentDateTime { get; set; }
+    }
+}
