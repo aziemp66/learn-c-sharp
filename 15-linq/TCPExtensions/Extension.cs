a026a7d4dba86b293568b3d5f922264cf0550e08
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-17 13:35:52 +0700

    Creating TCP Extensions

---
+++ 15-linq/TCPExtensions/Extension.cs
@@ -0,0 +1,19 @@
+namespace tcp_extensions;
+
+using System.Collections.Generic;
+
+public static class Extension
+{
+    public static List<T> Filter<T>(this List<T> records, Predicate<T> func)
+    {
+        var filteredList = new List<T>();
+        foreach (T record in records)
+        {
+            if (func(record))
+            {
+                filteredList.Add(record);
+            }
+        }
+        return records;
+    }
+}
