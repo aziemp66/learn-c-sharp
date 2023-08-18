e5120e2d84d50dd3e4ce8bf3d3bd89e3af1ba0b2
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-18 07:38:08 +0700

    Another Deffered/Lazy Execution and Immediate Execution Examples

--- 16-linq-queries/Main.cs
+++ 16-linq-queries/Main.cs
@@ -4,6 +4,25 @@ using TCPData;
 using TCPExtensions;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
+using System.Threading;
+
+public static class EnumerableCollectionExtensionsMethods
+{
+    public static IEnumerable<Employee> GetHighSalariedEmployees(
+        this IEnumerable<Employee> employees
+    )
+    {
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"Accessing employee : {employee.FirstName} {employee.LastName}");
+            if (employee.AnnualSalary >= 50_000m)
+            {
+                yield return employee;
+            }
+        }
+    }
+}
 
 class Program
 {
@@ -25,16 +44,50 @@ class Program
         // }
 
         // Select and Where Operator - Query Syntax (Deffered Execution)
-        var results =
-            from emp in employeeList
-            where emp.AnnualSalary > 50_000m
+        // var results =
+        //     from emp in employeeList
+        //     where emp.AnnualSalary > 50_000m
+        //     select new
+        //     {
+        //         FullName = $"{emp.FirstName} {emp.LastName}",
+        //         AnnualSalary = emp.AnnualSalary
+        //     };
+        //
+        // // Employee will be added to results despite only added later because of Deffered Execution
+        // employeeList.Add(
+        //     new Employee
+        //     {
+        //         Id = 5,
+        //         FirstName = "Nabil",
+        //         LastName = "Juliano",
+        //         AnnualSalary = 150_000m
+        //     }
+        // );
+        //
+        // foreach (var item in results)
+        // {
+        //     Console.WriteLine($"{item.FullName,-20} : {item.AnnualSalary.ToString("n0"),10}");
+        // }
+
+        // Deferred Execution
+        // var results =
+        //     from emp in employeeList.GetHighSalariedEmployees()
+        //     select new
+        //     {
+        //         FullName = $"{emp.FirstName} {emp.LastName}",
+        //         AnnualSalary = emp.AnnualSalary
+        //     };
+
+        // Immediate Execution
+        var results = (
+            from emp in employeeList.GetHighSalariedEmployees()
             select new
             {
                 FullName = $"{emp.FirstName} {emp.LastName}",
                 AnnualSalary = emp.AnnualSalary
-            };
+            }
+        ).ToList();
 
-        // Employee will be added to results despite only added later because of Deffered Execution
         employeeList.Add(
             new Employee
             {
@@ -47,7 +100,9 @@ class Program
 
         foreach (var item in results)
         {
-            Console.WriteLine($"{item.FullName,-20} : {item.AnnualSalary,10}");
+            Console.WriteLine(
+                $"{item.FullName,-20} : {item.AnnualSalary.ToString("C0", new CultureInfo("fr-FR")),10}"
+            );
         }
     }
 }
